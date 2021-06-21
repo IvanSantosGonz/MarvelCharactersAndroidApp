c1a824d04e7d036a8a8a4c39cfa7c6feea0f186b
Ivan <Ivansantosgonz@gmail.com>
2021-06-21 18:12:18 +0100

    ✨ Add transition between SplashScreenActivity to MainActivity after two seconds

--- app/src/main/java/ivansantos/marvelcharacters/presentation/SplashScreenActivity.kt
+++ app/src/main/java/ivansantos/marvelcharacters/presentation/SplashScreenActivity.kt
@@ -1,12 +1,29 @@
 package ivansantos.marvelcharacters.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ivansantos.marvelcharacters.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        GlobalScope.launch {
+            val delayTimeMillis: Long = 2000
+            delay(delayTimeMillis)
+            goNextActivity()
+        }
+    }
+
+    private fun goNextActivity() {
+        val mainActivityIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainActivityIntent)
+        finish()
     }
 }
