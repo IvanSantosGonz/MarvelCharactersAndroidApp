a06b7744f3eb966d054e6d48adf161c126ca9995
Ivan <Ivansantosgonz@gmail.com>
2021-07-01 18:32:54 +0100

    ➕ add Android Hilt dependency

---
+++ app/src/main/java/ivansantos/marvelcharacters/MarvelCharactersApplication.kt
@@ -0,0 +1,7 @@
+package ivansantos.marvelcharacters
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MarvelCharactersApplication : Application()
