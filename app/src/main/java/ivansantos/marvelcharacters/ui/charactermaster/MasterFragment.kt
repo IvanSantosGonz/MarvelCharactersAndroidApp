65cf5ab411c9cebf41e0653e7108a5cb6c3e2b89
Iván Santos <IvanSantosGonz@gmail.com>
2022-01-02 10:40:17 +0000

    ♻️ REFACTOR: extract master to detail navigation to function

--- app/src/main/java/ivansantos/marvelcharacters/ui/charactermaster/MasterFragment.kt
+++ app/src/main/java/ivansantos/marvelcharacters/ui/charactermaster/MasterFragment.kt
@@ -20,6 +20,10 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MasterFragment : Fragment() {
 
+    //We use by activityViewModels instead of by viewModels because we want to share the viewmodel
+    //between master and detail fragment associated with the activity lifecycle (using by viewmodel
+    // it is associated to the fragment lifecycle)
+    private val marvelCharactersViewModel: MarvelCharactersViewModel by activityViewModels()
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: MarvelCharactersRecyclerViewAdapter
     private lateinit var fragmentMasterBinding: FragmentMasterBinding
@@ -39,32 +43,25 @@ class MasterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //We use by activityViewModels instead of by viewModels because we want to share the viewmodel
-        //between master and detail fragment associated with the activity lifecycle (using by viewmodel
-        // it is associated to the fragment lifecycle)
-        val marvelCharactersViewModel: MarvelCharactersViewModel by activityViewModels()
         fragmentMasterBinding.viewmodel = marvelCharactersViewModel
         fragmentMasterBinding.lifecycleOwner = this
 
-        val onClickListener = View.OnClickListener { itemView -> //TODO: move to function
-            val clickedCharacterPosition = recyclerView.getChildAdapterPosition(itemView)
-            val clickedMarvelCharacter =
-                marvelCharactersViewModel.characters.value?.get(clickedCharacterPosition)
-            clickedMarvelCharacter?.let { marvelCharactersViewModel.setSelected(it) }
-            itemView.findNavController().navigate(R.id.show_detail)
+        val onMarvelCharacterClickListener = View.OnClickListener { itemView ->
+            navigateToDetailFragment(itemView)
+        }
+        setAdapter(listOf(), onMarvelCharacterClickListener)
+
+        if (marvelCharactersViewModel.characters.value == null) { //TODO: extract to function
+            marvelCharactersViewModel.loadCharacters()
         }
 
         fragmentMasterBinding.retryButton.setOnClickListener {
             marvelCharactersViewModel.loadCharacters()
         }
 
-        setAdapter(listOf(), onClickListener)
-        if (marvelCharactersViewModel.characters.value == null) {
-            marvelCharactersViewModel.loadCharacters()
-        }
 
         marvelCharactersViewModel.characters.observe(
-            this,
+            viewLifecycleOwner,
             { characters ->
                 viewAdapter.swapData(characters)
             }
@@ -78,6 +75,14 @@ class MasterFragment : Fragment() {
                 marvelCharactersViewModel.loadCharacters()
             }
         }
+    }
+
+    private fun navigateToDetailFragment(itemView: View) {
+        val clickedCharacterPosition = recyclerView.getChildAdapterPosition(itemView)
+        val clickedMarvelCharacter =
+            marvelCharactersViewModel.characters.value?.get(clickedCharacterPosition)
+        clickedMarvelCharacter?.let { marvelCharactersViewModel.setSelected(it) }
+        itemView.findNavController().navigate(R.id.show_detail)
     }
 
     private fun setAdapter(
